ff55f48d236455f5b248dcce441ce8c79e887de6
Brett Odgers-Jewell <brett.odgers-jewell@konjur.sg>
2025-01-08 14:57:37 +0800

    update module

--- module.tf
+++ module.tf
@@ -1,4 +1,4 @@
 resource "azurerm_subnet_network_security_group_association" "snet_nsg_association" {
-  subnet_id                 = azurerm_subnet.example.id
-  network_security_group_id = azurerm_network_security_group.example.id
+  subnet_id                 = var.subnet_id
+  network_security_group_id = var.network_security_group_id
 }
