3abf11b4bf695bb787ce6b2c772b91b9893d6ac7
Brett Odgers-Jewell <brett.odgers-jewell@konjur.sg>
2025-01-08 14:55:31 +0800

    update module

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable "network_security_group_id`" {
+variable "network_security_group_id" {
   description = "(Required) The ID of the Network Security Group which should be associated with the Subnet. Changing this forces a new resource to be created."
   type = string
 } 
